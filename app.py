ed07a57c244206eba66ce84550cd75db23c4300d
William Dennis <wwdennis.home@gmail.com>
2026-01-02 22:12:32 +0000

    adding data

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import math
-import random
-import yfinance as yf
+
+from data_downloader import get_two_series
 
 st.write("Hi!")
+
+data = get_two_series()
+
+st.dataframe(data)
---
+++ data_downloader.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import pandas as pd
+
+def get_daily_data(ticker_symbol: str, period="10y"):
+    ticker = yf.Ticker(ticker_symbol)
+    close_series = ticker.history(period=period)["Close"]
+    close_series.name = ticker_symbol
+    return close_series
+
+def get_two_series(ticker1: str = "SPY", ticker2: str = "GLD", period="10y"):
+    s1 = get_daily_data(ticker1, period)
+    s2 = get_daily_data(ticker2, period)
+
+    # normalise
+    s1 = s1 / s1.values[0]
+    s2 = s2 / s2.values[0]
+
+    joined = pd.concat([s1, s2], axis=1).dropna()
+    return joined
